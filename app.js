d3b830d03b34146257be52af6b89e78da907b566
David Hatch <hatchdavidv@gmail.com>
2022-12-27 18:29:10 -0600

    images-and-design

--- app.js
+++ app.js
@@ -6,33 +6,15 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('/', (req, res, next)=>{
-    res.send('Welcome new visitor, this is a test webpage') // Para mandar únicamente texto 
+    res.send('Welcome new visitor, this webpage will show you some information about Isaac Newton') // Para mandar únicamente texto 
 })
 
 app.get('/pagina1', (req, res, next)=>{
     res.send(`<h1> Título de prueba </h1>`)
 })
 
-app.get('/basicInfo1', (req, res, next)=>{
-    res.send(`
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Basic Info</title>
-</head>
-<body>
-     <h1>Artist Basic Info </h1>
-      <ul>
-         <li>Name: David </li>
-         <li>Last Name: Hatch </li>
-         <li>LinkedIn: https://www.linkedin.com/in/david-hatch-vazquez/ </li>
-      </ul>
-</body>
-</html>
-    `)
+app.get('/profile', (req, res, next)=>{
+    res.sendFile(__dirname + '/views/perfil.html' )
 })
 
 app.listen(3000, ()=>{
